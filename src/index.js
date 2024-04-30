44afbf603074a53e83167ad68afb9b874c20dac6
Jmendoti <jmendoti21@gmail.com>
2024-04-29 21:48:16 -0500

    cambios generados en  en index back

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
-import  {connectDB}  from "./database.js";
+import { connectDB } from "./database.js";
 
+// Rest API
+app.get("/", (req, res) => {
+    res.send("<h1>Bienvenidos Server Corriendo</h1>");
+});
 connectDB();
 app.listen(4000)
 console.log('Conectado al puerto ', 4000)
